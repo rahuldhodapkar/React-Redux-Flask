8f8d98af6221f70d1f05faef9f5c4dc4ca272b53
Rahul Dhodapkar <rahuldhodapkar@Rahuls-MacBook-Pro.local>
2018-02-21 18:55:38 -0500

    update nameing to fix npm install issues

--- static/webpack/prod.config.js
+++ static/webpack/prod.config.js
@@ -26,7 +26,7 @@ module.exports = {
         }),
         new ExtractTextPlugin('bundle.css'),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
